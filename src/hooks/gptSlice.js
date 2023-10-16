db10644bafe188c4552617aeb6a24eed765f41e3
maroof sofi <maroofsofi99@gmail.com>
2023-10-16 11:44:30 +0530

    gptSlice created

---
+++ src/hooks/gptSlice.js
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const gptSlice = createSlice({
+    name : 'gpt',
+    initialState:{
+         showGptSearch : false,
+         gptMovies : null
+    },
+    reducers:{
+        toggleGptSearchView :(state)=>{
+            state.showGptSearch = !state.showGptSearch;
+        },
+        addGptMovieResult : (state, action)=>{
+         state.gptMovies = action.payload;
+
+        },
+
+    },
+});
+export const {toggleGptSearchView, addGptMovieResult} = gptSlice.actions;
+
+export default gptSlice.reducer;
